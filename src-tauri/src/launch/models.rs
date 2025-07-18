693eef7ad715bd8b0175c8fc4d6af516ad5f3a6e
Yujie Sun <sunyujie@sjtu.edu.cn>
2025-07-18 11:34:56 +0800

    fix(resource): fix neoforge resource sort error in bmclapi 1.20.1

--- src-tauri/src/launch/models.rs
+++ src-tauri/src/launch/models.rs
@@ -9,7 +9,6 @@ use strum_macros::Display;
 #[derive(Debug, Display)]
 #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
 pub enum LaunchError {
-  DirectoryNotFound,
   NoSuitableJava,
   GameFilesIncomplete,
   SetProcessPriorityFailed,
